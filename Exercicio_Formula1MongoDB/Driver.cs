765120efbd619cfefb51c453ed9da308e4036f2c
christian-777 <60942806+christian-777@users.noreply.github.com>
2023-04-12 13:37:08 -0300

    Add project files.

---
+++ Exercicio_Formula1MongoDB/Driver.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace Exercicio_Formula1MongoDB
+{
+    internal class Driver
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string? Id { get; set; }
+        public string?  Name { get; set; }
+        public string? Abbreviation { get; set; }
+        public int Number{ get; set; }
+        public string? Team { get; set; }
+        public string? Country { get; set; }
+        public string? BirthDate { get; set; }
+
+        public Driver(string? name, string? abbreviation, int number, string? team, string? country, string? birthDate)
+        {
+            Name = name;
+            Abbreviation = abbreviation;
+            Number = number;
+            Team = team;
+            Country = country;
+            BirthDate = birthDate;
+        }
+    }
+}
---
+++ Exercicio_Formula1MongoDB/Program.cs
@@ -0,0 +1,57 @@
+﻿using Exercicio_Formula1MongoDB;
+using MongoDB.Bson;
+using MongoDB.Driver;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        MongoClient mongo = new MongoClient("mongodb://localhost:27017");
+
+        var basededados = mongo.GetDatabase("Formula1");
+        var collections = basededados.GetCollection<BsonDocument>("Pilotos");
+        var documents = collections.Find(new BsonDocument()).ToList();
+
+        Console.WriteLine("nome: ");
+        string n= Console.ReadLine();
+
+        Console.WriteLine("Abreviacao: ");
+        string a = Console.ReadLine();
+
+        Console.WriteLine("numero: ");
+        int num = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("equipe: ");
+        string t = Console.ReadLine();
+
+        Console.WriteLine("pais: ");
+        string c = Console.ReadLine();
+
+        Console.WriteLine("data de nascimento: ");
+        string b = Console.ReadLine();
+
+        Driver d = new(n, a, num, t, c, b);
+
+        /*Console.WriteLine("informe o nome do piloto: ");
+        var p= Console.ReadLine();
+
+        var filtro = Builders<BsonDocument>.Filter.Regex("Driver", p);
+
+        var piloto = collections.Find(filtro).FirstOrDefault();
+
+        Console.WriteLine(piloto);
+
+        Console.WriteLine("informe o nome da equipe: ");
+        var e = Console.ReadLine();
+
+        filtro = Builders<BsonDocument>.Filter.Regex("Team", e);
+
+        var pilotos = collections.Find(filtro).ToList();
+
+        foreach(var driver in pilotos)
+        {
+            Console.WriteLine(driver);
+        }*/
+
+    }
+}
